307f2d5cfca3e361101d67d2e3cc1b3cc9f06a64
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2025-12-07 12:52:43 +0100

    ME-8: Create ITexture interface (Getters for width, height, format).

--- Mixture/include/Mixture/Render/RHI/RHI.hpp
+++ Mixture/include/Mixture/Render/RHI/RHI.hpp
@@ -3,3 +3,5 @@
 #include "Mixture/Render/RHI/RenderFormats.hpp"
 #include "Mixture/Render/RHI/RenderStates.hpp"
 #include "Mixture/Render/RHI/ResourceStates.hpp"
+
+#include "Mixture/Render/RHI/ITexture.hpp"
