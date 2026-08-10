7e4e5a38542136cf4c085f808486d0e6a5ee2cce
Maximilian Bollschweiler <maxi.bollschweiler@gmail.com>
2026-08-10 16:21:28 +0200

    feat: integrate cli into the setup scripts

--- Scripts/python/Setup.py
+++ Scripts/python/Setup.py
@@ -1,1 +1,7 @@
-from mixture_tools.setup import runner  # noqa: F401
+import sys
+
+from mixture_tools.cli import main
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:] or ["setup"]
+    raise SystemExit(main(arguments))
